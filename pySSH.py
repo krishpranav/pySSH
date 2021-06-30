dd6974b63889dc06d6f2f74a1d13486ccf8bd089
krishpranav <krisna.pranav@gmail.com>
2021-06-30 18:54:04 +0530

    [+] check auth password [+]

--- pySSH.py
+++ pySSH.py
@@ -7,3 +7,9 @@ import argparse
 import logging 
 
 import paramiko
+
+
+class SSH_Server(paramiko.ServerInterface):
+    def check_auth_password(self, user, passwd):
+        print "[*] Connection Attempt: user=> " + str(user) + " | passwd=> " + str(passwd)
+        return paramiko.AUTH_FAILED
